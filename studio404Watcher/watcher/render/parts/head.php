bf4f48645382049da38b84ecf24c8513bb426bc4
master <hosting@404.ge>
2019-01-30 12:31:30 -0600

    save 2

--- studio404Watcher/watcher/render/parts/head.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>Studio 404 - Watcher</title>
-<link href="watcher/render/css/stylesheet.css" type="text/css" rel="stylesheet" />
-</head>
-<body>
